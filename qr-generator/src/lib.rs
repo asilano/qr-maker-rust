de4e5aa3365437c6c49e93cf8a54d3fde74fd856
Chris Howlett <chris.howlett@freeagent.com>
2022-12-04 22:40:29 +0000

    Add timing patterns. Fix no. of finders in Micro

--- qr-generator/src/lib.rs
+++ qr-generator/src/lib.rs
@@ -1,22 +1,24 @@
 mod qr_types;
 use std::error::Error;
-use image::{ImageBuffer, GrayImage, imageops};
-use qr_types::{QRFactory, QRSymbolTypes};
+use image::{ImageBuffer, GrayImage, Luma, imageops};
+use qr_types::{QRFactory, QRSymbolTypes, FinderLocations};
 
 pub fn save_qr_image(filepath: &String) -> Result<(), Box<dyn Error>> {
-    let qr_code = QRFactory::build_code(QRSymbolTypes::QRCode, 1);
+    let qr_code = QRFactory::build_code(QRSymbolTypes::QRCode, 4);
 
     let dimension = qr_code.module_width();
-    let mut loud_region: GrayImage = ImageBuffer::from_pixel(dimension, dimension, image::Luma([0]));
-    add_finder_patterns(&mut loud_region);
+    let mut loud_region: GrayImage = ImageBuffer::from_pixel(dimension, dimension, Luma([128]));
+
+    add_timing_patterns(&mut loud_region, qr_code.timing_coord());
+    add_finder_patterns(&mut loud_region, qr_code.finder_locations());
 
     let quiet_width = 4;
     let full_dimension = dimension + quiet_width * 2;
     let mut full_image: GrayImage = ImageBuffer::from_fn(full_dimension, full_dimension, |x, y| {
         if x < quiet_width || x >= dimension || y < quiet_width || y >= dimension {
-            image::Luma([255])
+            Luma([255])
         } else {
-            image::Luma([128])
+            Luma([128])
         }
     });
 
@@ -29,25 +31,38 @@ pub fn save_qr_image(filepath: &String) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-fn add_finder_patterns(buffer: &mut GrayImage) {
-    add_finder_pattern(buffer, 0, 0);
-    add_finder_pattern(buffer, 0, buffer.height() as i64 - 7);
-    add_finder_pattern(buffer, buffer.width() as i64 - 7, 0);
+fn add_timing_patterns(buffer: &mut GrayImage, timing_coord: u32) {
+    let horiz: GrayImage = ImageBuffer::from_fn(buffer.width(), 1, |x, _| {
+        if x % 2 == 0 { Luma([0]) } else { Luma([255]) }
+    });
+
+    imageops::overlay(buffer, &horiz, 0, timing_coord as i64);
+    imageops::overlay(buffer, &imageops::rotate90(&horiz), timing_coord as i64, 0);
+}
+
+fn add_finder_patterns(buffer: &mut GrayImage, locations: Vec<FinderLocations>) {
+    for location in locations {
+        match location {
+            FinderLocations::TopLeft => add_finder_pattern(buffer, 0, 0),
+            FinderLocations::BottomLeft => add_finder_pattern(buffer, 0, buffer.height() as i64 - 7),
+            FinderLocations::TopRight => add_finder_pattern(buffer, buffer.width() as i64 - 7, 0)
+        }
+    }
 }
 
 fn add_finder_pattern(buffer: &mut GrayImage, left: i64, top: i64) {
     let finder: GrayImage = ImageBuffer::from_fn(9, 9, |x, y| {
         if x == 0 || y == 0 || x == 8 || y == 8 {
-            image::Luma([255])
+            Luma([255])
         }
         else if x == 1 || y == 1 || x == 7 || y == 7 {
-            image::Luma([0])
+            Luma([0])
         }
         else if x == 2 || y == 2 || x == 6 || y == 6 {
-            image::Luma([255])
+            Luma([255])
         }
         else {
-            image::Luma([0])
+            Luma([0])
         }
     });
 
--- qr-generator/src/qr_types.rs
+++ qr-generator/src/qr_types.rs
@@ -1,5 +1,15 @@
+#[derive(Debug, PartialEq)]
+pub enum FinderLocations {
+    TopLeft,
+    TopRight,
+    BottomLeft
+}
+use FinderLocations::*;
+
 pub trait QRSymbol {
     fn module_width(&self) -> u32;
+    fn timing_coord(&self) -> u32;
+    fn finder_locations(&self) -> Vec<FinderLocations>;
 }
 
 pub struct QRCode {
@@ -9,6 +19,10 @@ impl QRSymbol for QRCode {
     fn module_width(&self) -> u32 {
         21 + 4 * (self.version - 1)
     }
+    fn timing_coord(&self) -> u32 { 6 }
+    fn finder_locations(&self) -> Vec<FinderLocations> {
+        vec![TopLeft, TopRight, BottomLeft]
+    }
 }
 
 pub struct MicroQRCode {
@@ -17,6 +31,10 @@ pub struct MicroQRCode {
 impl QRSymbol for MicroQRCode {
     fn module_width(&self) -> u32 {
         11 + 2 * (self.version - 1)
+    }
+    fn timing_coord(&self) -> u32 { 0 }
+    fn finder_locations(&self) -> Vec<FinderLocations> {
+        vec![TopLeft]
     }
 }
 
@@ -62,5 +80,23 @@ mod tests {
     #[test]
     fn micro_qr_code_has_correct_width_v4() {
         assert_eq!(MicroQRCode {version: 4}.module_width(), 17);
+    }
+
+    #[test]
+    fn qr_code_timing_in_correct_place() {
+        assert_eq!(QRCode {version: 5}.timing_coord(), 6);
+    }
+    #[test]
+    fn micro_qr_code_timing_in_correct_place() {
+        assert_eq!(MicroQRCode {version: 3}.timing_coord(), 0);
+    }
+
+    #[test]
+    fn qr_code_three_finders() {
+        assert_eq!(QRCode {version: 15}.finder_locations(), vec![TopLeft, TopRight, BottomLeft]);
+    }
+    #[test]
+    fn micro_qr_code_one_finder() {
+        assert_eq!(MicroQRCode {version: 1}.finder_locations(), vec![TopLeft]);
     }
 }
