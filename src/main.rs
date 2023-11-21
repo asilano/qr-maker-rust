4f75a5626c026e5858816ef2381353616198d1ab
Chris Howlett <chris.howlett@freeagent.com>
2023-11-21 22:37:49 +0000

    Complete EC blocks in sizer

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,12 @@ use std::process;
 fn main() {
     let options = qr_generator::Options {
         // mode: Some(EncodingModes::Numeric),
-        // version: Some(8),
-        // correction_level: Some(CorrectionLevels::H),
+        // version: Some(30),
+        correction_level: Some(CorrectionLevels::H),
         ..Default::default()
     };
     let mut generator = QRGenerator { options };
-    let ret = generator.make_qr_code("@123456".to_string());
+    let ret = generator.make_qr_code("Sing, O goddess, the anger of Achilles son of Peleus, that brought countless ills upon the Achaeans.".to_string());
     if let Err(err) = ret {
         println!("save_qr_image failed with {}", err);
         process::exit(1);
